e35597fe2ec2a063119c95e20787dd43dcb66bf8
AtharvaAnilChanda <92516073+AtharvaAnilChanda@users.noreply.github.com>
2023-01-03 16:17:37 +0530

    Create Populating_Database.sql

---
+++ Populating_Database.sql
@@ -0,0 +1,12 @@
+INSERT INTO DIRECTORS (Director_ID , D_Name)
+VALUES ('0001', 'James Cameron');
+INSERT INTO DIRECTORS (Director_ID , D_Name)
+VALUES ('0002', 'Steven Spielberg');
+INSERT INTO DIRECTORS (Director_ID , D_Name)
+VALUES ('0003', 'Gore Verbinski');
+INSERT INTO DIRECTORS (Director_ID , D_Name)
+VALUES ('0004', 'Leslee Feldman');
+INSERT INTO DIRECTORS (Director_ID , D_Name)
+VALUES ('0005', 'Joe Russo');
+INSERT INTO DIRECTORS (Director_ID , D_Name)
+VALUES ('0006', 'Francis Ford Coppola');
