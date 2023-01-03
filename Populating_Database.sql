39f8808e13ebf4d5d00b2ea7c0c75bf18e6d029d
AtharvaAnilChanda <92516073+AtharvaAnilChanda@users.noreply.github.com>
2023-01-03 16:18:19 +0530

    Update Populating_Database.sql

--- Populating_Database.sql
+++ Populating_Database.sql
@@ -10,3 +10,15 @@ INSERT INTO DIRECTORS (Director_ID , D_Name)
 VALUES ('0005', 'Joe Russo');
 INSERT INTO DIRECTORS (Director_ID , D_Name)
 VALUES ('0006', 'Francis Ford Coppola');
+
+
+INSERT INTO PRODUCTION_COMPANY (PC_ID , P_Name)
+VALUES ('0001', 'The Walt Disney Company');
+INSERT INTO PRODUCTION_COMPANY (PC_ID , P_Name)
+VALUES ('0002', ' Dreamworks');
+INSERT INTO PRODUCTION_COMPANY (PC_ID , P_Name)
+VALUES ('0003', 'Paramount Pictures');
+INSERT INTO PRODUCTION_COMPANY (PC_ID , P_Name)
+VALUES ('0004', 'Marvel Studios');
+INSERT INTO PRODUCTION_COMPANY (PC_ID , P_Name)
+VALUES ('0005', 'Park Circus Films France');
