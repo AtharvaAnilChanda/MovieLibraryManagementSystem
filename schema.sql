6655e3c8efb42ff4fbd39b6ef4a70b9517f9a54f
AtharvaAnilChanda <92516073+AtharvaAnilChanda@users.noreply.github.com>
2023-01-03 16:09:33 +0530

    Create schema.sql
    DDL commands . Defining the schema .

---
+++ schema.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE Movie_lib;
+USE Movie_lib
+
+--- Movie_lib ---
+
+CREATE TABLE MOVIES(
+Movie_ID int(5) NOT NULL AUTO_INCREMENT,
+Title varchar(255) NOT NULL,
+Release_year varchar(4),
+Genre varchar(50),
+Rating float,
+Director_ID int(4) NOT NULL ,
+PC_ID int(4) NOT NULL ,
+PRIMARY KEY (Movie_ID)
+);
+
+CREATE TABLE ACTORS(
+Actor_ID int(4) NOT NULL AUTO_INCREMENT,
+A_Name varchar(255) NOT NULL,
+PRIMARY KEY (Actor_ID)
+);
+
+CREATE TABLE DIRECTORS(
+Director_ID int(4) NOT NULL AUTO_INCREMENT,
+D_Name varchar(255) NOT NULL,
+PRIMARY KEY (Director_ID)
+);
+
+CREATE TABLE PRODUCTION_COMPANY(
+PC_ID int(4) NOT NULL AUTO_INCREMENT,
+P_Name varchar(255) NOT NULL,
+PRIMARY KEY (PC_ID)
+);
+
+CREATE TABLE ACTS(
+Actor_ID int(4) NOT NULL ,
+Movie_ID int(5) NOT NULL ,
+A_Role varchar(10) ,
+FOREIGN KEY (Movie_ID) REFERENCES MOVIES(Movie_ID),
+FOREIGN KEY (Actor_ID) REFERENCES ACTORS(Actor_ID)
+);
+
+
