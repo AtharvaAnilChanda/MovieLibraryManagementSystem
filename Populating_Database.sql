b3cb95a7b52449a2c4d26b100f2954b6faed4a56
AtharvaAnilChanda <92516073+AtharvaAnilChanda@users.noreply.github.com>
2023-01-03 16:18:51 +0530

    Update Populating_Database.sql

--- Populating_Database.sql
+++ Populating_Database.sql
@@ -22,3 +22,47 @@ INSERT INTO PRODUCTION_COMPANY (PC_ID , P_Name)
 VALUES ('0004', 'Marvel Studios');
 INSERT INTO PRODUCTION_COMPANY (PC_ID , P_Name)
 VALUES ('0005', 'Park Circus Films France');
+
+
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0001', 'Leonardo Di Caprio');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0002', 'Tom Hanks');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0003', 'Kate Winslet');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0004', 'Johnny Depp');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0005', 'Orlando Bloom');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0006', 'Kiera Knightly');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0007', 'Jackie Chan');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0008', 'Angelina Jolie');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0009', 'Jack Black');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0010', 'Robert Downy Jr');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0011', 'Meryl Streep');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0012', 'Chris Evans');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0013', 'Chris Hemsworth');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0014', 'Scarlett Johansson');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0015', 'Gwyneth Paltrow');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0016', 'Al Pacino');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0017', 'Robert De Niro');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0018', 'Talia Shire');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0019', 'Jennifer Garner');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0020', 'Matt Damon');
+INSERT INTO ACTORS (Actor_ID , A_Name)
+VALUES ('0021', 'Abigail Breslin');
