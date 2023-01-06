8dab0916ba2a30e7f7a9c9efbb7ff33fbe1c1b99
AtharvaAnilChanda <92516073+AtharvaAnilChanda@users.noreply.github.com>
2023-01-07 00:39:41 +0530

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,25 +1,17 @@
 
 --- Trigger
 
-drop trigger if exists check_null_A_Role ;
+drop trigger if exists check_null_A_Role_1 ;
 
-CREATE TABLE MESSAGE(
-    id int AUTO_INCREMENT,
-    message_Id int ,
-    message varchar(300) not null ,
-    primary key (id , messageId) );
-
-
-    Delimiter // 
+Delimiter // 
     create trigger
-    check_null_A_Role
-    after insert 
+    check_null_A_Role_1
+    before insert 
     on ACTS
     for each row 
     begin 
     if new.A_Role is null then 
-    insert into message (message_ID , message )
-    values(new.id , concat(" Actor role should not be left null "));
+        signal sqlstate '50001' set MESSAGE_TEXT = ' Actor role should not be left null'
     end if ;
     end //
     delimiter ;
