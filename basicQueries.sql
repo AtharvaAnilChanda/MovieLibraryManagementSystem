b984050d9d4b49e9c8bc29a0ed9ce7d5aa8b2f76
AtharvaAnilChanda <92516073+AtharvaAnilChanda@users.noreply.github.com>
2023-01-06 23:55:04 +0530

    Update basicQueries.sql

--- basicQueries.sql
+++ basicQueries.sql
@@ -2,3 +2,7 @@
 
 --- view_all_movies():
     SELECT * FROM MOVIES
+
+--- view_by_director():
+   SELECT MOVIES.movie_id , MOVIES.Title FROM MOVIES WHERE Director_ID IN ( SELECT Director_ID from DIRECTORS WHERE D_Name = "Steven Spielberg" )
+    
