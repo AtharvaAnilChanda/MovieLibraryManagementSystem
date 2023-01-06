26a1b5c2489a1390bf7d400d8fb6eecdfcaa5cb9
AtharvaAnilChanda <92516073+AtharvaAnilChanda@users.noreply.github.com>
2023-01-06 23:56:18 +0530

    Update basicQueries.sql

--- basicQueries.sql
+++ basicQueries.sql
@@ -9,9 +9,18 @@
 --- view_by_production_company():
     SELECT MOVIES.Movie_ID , MOVIES.Title FROM MOVIES WHERE PC_ID IN (SELECT PC_ID FROM PRODUCTION_COMPANY WHERE P_Name = "Dreamworks" )
     
-
 -- view_by_Actors():
     
 SELECT MOVIES.Movie_ID , MOVIES.Title FROM MOVIES WHERE Movie_ID IN
      (SELECT Movie_ID FROM ACTS WHERE Actor_ID in (SELECT Actor_ID from actors WHERE A_Name = "Tom Hanks"));
     
+--- def view_movie_cast():
+
+    SELECT A.A_Name , B.A_Role 
+	FROM ACTORS A, ACTS B,MOVIES C
+	where 
+	A.Actor_ID = B.Actor_ID AND 
+	B.Movie_ID = C.Movie_ID AND
+    C.Title  = " Avengers "  
+    data = c.fetchall()
+    return data 
